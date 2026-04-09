ce9551d79b155063b02f64034040c68619a16fe7
Niveyy-Git <your-github-email>
2026-04-09 22:08:59 +0530

    added Task4 program

---
+++ Task4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main ()
+{
+  int a,b;
+  printf("Enter the Nos : ");
+  scanf("%d%d",&a,&b);
+if(a>b)
+{
+  printf(" a is max ");
+}
+if(b>a)
+{
+  printf("b is max ");
+}
+}
